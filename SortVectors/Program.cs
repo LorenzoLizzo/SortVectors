1761793450689b007b5d24271136ae471a3e7318
Lorenzo Lizzo <LORENZO.LIZZO@local.ittcesena.it>
2020-10-07 11:07:02 +0200

    First step

---
+++ SortVectors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SortVectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vett = new int[10] { 1, -1, -5, -2, 4, -6, 7, 8, 9, -10 };
+            for (int i = 0; i < vett.Length; i++)
+            {
+                Console.WriteLine(vett[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
