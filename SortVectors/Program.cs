c577c58a4c43e1cc9ee38d8a6d59ccae521574fa
Lorenzo Lizzo <LORENZO.LIZZO@local.ittcesena.it>
2020-10-07 11:47:25 +0200

    Second Step Ordinamento

--- SortVectors/Program.cs
+++ SortVectors/Program.cs
@@ -7,11 +7,19 @@ namespace SortVectors
         static void Main(string[] args)
         {
             int[] vett = new int[10] { 1, -1, -5, -2, 4, -6, 7, 8, 9, -10 };
+
+            Ordina(vett);
+
             for (int i = 0; i < vett.Length; i++)
             {
                 Console.WriteLine(vett[i]);
             }
             Console.ReadKey();
+        }
+
+        static void Ordina(int[] v)
+        {
+            Array.Sort(v);
         }
     }
 }
